614ffc2cca134cf48f2444919bcf4b5d05feef80
EugeniaRe <evrepni@gmail.com>
2023-10-02 15:37:48 +0500

    complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+  const hundred = Math.floor(n/100);
+  const dec = Math.floor((n%100)/10);
+  const unit = n%10;
+
+  return (unit*100 + dec*10 + hundred);
 }
